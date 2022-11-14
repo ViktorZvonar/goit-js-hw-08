d91d9ce69c8a2d1a9c73fff1a3718560629a6ef9
ViktorZvonar <viktorzvonar@yahoo.com>
2022-11-14 23:14:29 +0200

    task one done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector('.gallery');
+console.log(ulEl);
+
+const createGallaryMarkup = galleryItems => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}"
+       
+        />
+        </a>
+    </div>
+    `;
+    })
+    .join('');
+};
+
+const gallaryMarkup = createGallaryMarkup(galleryItems);
+ulEl.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+const onDivElClick = event => {
+  event.preventDefault();
+  const isPicture = event.target.classList.contains('gallery__image');
+
+  if (!isPicture) {
+    return;
+  }
+};
+
+ulEl.addEventListener('click', onDivElClick);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
