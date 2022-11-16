a1bcfcb08b3dbcdfc3111a950678c4f6b1f5d8cb
ViktorZvonar <viktorzvonar@yahoo.com>
2022-11-16 09:50:54 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,43 @@
-import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const options = {
+  duration: 61.857,
+  percent: 0.049,
+  seconds: 3.034,
+};
+const player = new Player(iframe, options);
+
+const onTimeUpdate = function ({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+const parsedTime = JSON.parse(savedTime);
+
+player
+  .setCurrentTime(parsedTime)
+  .then(function (time) {
+    if (parsedTime) {
+      time = parsedTime;
+    }
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
